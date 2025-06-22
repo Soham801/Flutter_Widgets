454d95f9a6d38ace36644e30162c6e1832982dc3
Soham801 <sohamdeshmukh801@gmail.com>
2025-06-22 11:53:10 +0530

    Modified Alert Widget

--- thirty_widgets/lib/widgets/alert.dart
+++ thirty_widgets/lib/widgets/alert.dart
@@ -6,15 +6,41 @@ class AlertWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Alert Dialog'),
-      ),
+      appBar: AppBar(title: Text('Alert Dialog')),
       body: Center(
         child: ElevatedButton(
-          onPressed: (){},
+          onPressed: () {
+            _showMyDialog(context);
+          },
           child: Text('Show Alert'),
-          ),
+        ),
       ),
     );
   }
+}
+
+Future<void> _showMyDialog(BuildContext context) async {
+  return showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        // backgroundColor: Colors.black,
+        title: Text('This is an Alert'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: [Text("This is a Demo"), Text('This is by the User')],
+          ),
+        ),
+        actions: [
+          TextButton(onPressed: () {}, child: Text('Aprrove')),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Cancel'),
+          ),
+        ],
+      );
+    },
+  );
 }
