e94aae4b3fee39203df9a6b55582ab819f769d76
Soham801 <sohamdeshmukh801@gmail.com>
2026-02-04 15:28:29 +0530

    Added Data types and Variables notes

---
+++ thirty_widgets/lib/dart/variable_datatypes.dart
@@ -0,0 +1,35 @@
+import 'dart:ffi';
+import 'dart:io';
+
+void main() {
+  // Declaration of Variable
+
+  int a;
+
+  // Assigning value to the variable
+
+  a = 5; // initialization
+  print(a);
+
+  // Assigning a large integar value to a variable
+
+  BigInt longvalue;
+  longvalue = BigInt.parse('7324565678877665555444');
+  print(longvalue);
+
+  // Double
+  double percentage;
+  percentage = 40;
+  print(percentage);
+
+  // num can also be used in the place of double as it can store int and double
+
+  // inline declarationcof  a string
+  String name = "Soham";
+  print(name);
+
+  // Boolean value
+
+  bool isLogin = true;
+  print(isLogin);
+}
