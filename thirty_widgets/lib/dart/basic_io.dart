076ba1abcc0012d5082f7637c60f9262d6397e2b
Soham801 <sohamdeshmukh801@gmail.com>
2026-02-06 23:07:56 +0530

    Done with var_dynamic

--- thirty_widgets/lib/dart/basic_io.dart
+++ thirty_widgets/lib/dart/basic_io.dart
@@ -6,7 +6,7 @@ void main() {
   // Basic Input and Output commands
   
   print("Welcome to Dart Day 1!!");
-  stdout.write("Enter your name: "); 
+  stdout.write("Enter your name: "); // Can be used instead of Print()
   var name = stdin.readLineSync();// Takes input from user
   print("Welcome $name, I hope you study well");
 
